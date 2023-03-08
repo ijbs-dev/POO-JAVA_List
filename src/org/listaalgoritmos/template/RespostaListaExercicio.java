38e9fb295fc3862d4405d0a7c91bed23e7e27ce7
Ismael Jefte <ismael_god@hotmail.com>
2023-03-08 18:27:19 -0300

    Adicionando Respostas Lista POO

--- src/org/listaalgoritmos/template/RespostaListaExercicio.java
+++ src/org/listaalgoritmos/template/RespostaListaExercicio.java
@@ -1,144 +1,250 @@
 package org.listaalgoritmos.template;
 
-
-
 public class RespostaListaExercicio {
-	
+
 	/*
-	 * Utilize apenas o método main para testar a implementação dos métodos. 
+	 * Utilize apenas o método main array para testar a implementação dos métodos.
 	 * Não se faz necessário implementar dentro do escopo do método.
 	 */
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 	/**
 	 * Método deve calcular o IMC utilizando peso e altura. Como resultado
 	 * o método deve retornar uma variável resultado do tipo float.
-	 * @param peso
-	 * @param altura
+	 * 
+	 * @arrayam peso
+	 * @arrayam altura
 	 * @return resultado
 	 */
-	public static float calcularImc(float peso, float altura) 
-	{
-		return 0f;
+	public static float calcularImc(float peso, float altura) {
+
+		float resultado = peso / (altura * altura);
+
+		return resultado;
 	}
-	
+
 	/**
 	 * Método deve calcular a área do trapézio independente da unidade de medida.
 	 * Como resultado, o método deve retornar a área calculada.
-	 * @param baseMaior
-	 * @param baseMenor
-	 * @param altura
+	 * 
+	 * @arrayam baseMaior
+	 * @arrayam baseMenor
+	 * @arrayam altura
 	 * @return areaTrapezio
 	 */
-	public static float calcularAreaTrapezio(float baseMaior, float baseMenor, float altura) 
-	{	
-		return 0f;
+	public static float calcularAreaTrapezio(float baseMaior, float baseMenor, float altura) {
+
+		float areaTrapezio = ((baseMaior + baseMenor) * altura) / 2;
+
+		return areaTrapezio;
 	}
-	
+
 	/**
 	 * O método deve calcular o maior entre dois inteiros, que são fornecidos
 	 * como argumento do método. O retorno deve ser o maior valor dado
 	 * como entrada, sendo do tipo inteiro.
-	 * @param a
-	 * @param b
+	 * 
+	 * @arrayam a
+	 * @arrayam b
 	 * @return maiorValor
 	 */
-	public static int maiorEntreDoisInteiros(int a, int b) 
-	{
-		return 0;
+	public static int maiorEntreDoisInteiros(int a, int b) {
+
+		int maiorValor;
+
+		if (a > b) {
+			maiorValor = a;
+		} else
+			maiorValor = b;
+
+		return maiorValor;
 	}
-	
+
 	/**
 	 * Método verifica se o número apresentado como argumento é ímpar ou par.
-	 * caso seja par, o retorno do método deve ser verdadeiro. Caso contrário,
+	 * caso seja array, o retorno do método deve ser verdadeiro. Caso contrário,
 	 * o valor retornado deve ser falso.
-	 * @param numero
+	 * 
+	 * @arrayam numero
 	 * @return condicao
 	 */
-	public static boolean verificarParImpar(int numero) 
-	{
-		return false;		
+	public static boolean verificarParImpar(int numero) {
+
+		boolean condicao = false;
+
+		if (numero % 2 == 0) {
+			condicao = true;
+		} else
+			condicao = false;
+
+		return condicao;
 	}
-	
+
 	/**
-	 * Método calcula a média aritmetica dos elementos float a partir de um array. 
+	 * Método calcula a média aritmetica dos elementos float a arraytir de um array.
 	 * O retorno deve ser o resultado do cálculo da média.
-	 * @param notas
+	 * 
+	 * @arrayam notas
 	 * @return media
 	 */
 	public static float calcularMediaNotas(float[] notas) {
-		return 0f;
+
+		float lista[] = new float[notas.length];
+		float soma = 0;
+
+		for (int i = 0; i < lista.length; i++) {
+			lista[i] = notas[i];
+			soma = soma + lista[i];
+		}
+
+		float media = soma / lista.length;
+		return media;
 	}
-	
+
 	/**
-	 * Método imprime array de inteiros no sentido inverso, separando os elementos por vírgula.
-	 * O último elemento impresso não deve preceder vírgula. Use o System.out.print() para imprimir os valores.
+	 * Método imprime array de inteiros no sentido inverso, searrayando os elementos
+	 * por vírgula.
+	 * O último elemento impresso não deve preceder vírgula. Use o
+	 * System.out.print() arraya imprimir os valores.
 	 * Os elementos devem ser impresso numa única linha.
-	 * @param array
+	 * 
+	 * @arrayam array
 	 */
-	public static void imprimirArrayInverso(int[] array)
-	{
-		System.out.print("");
+	public static void imprimirArrayInverso(int[] array) {
+
+		for (int i = array.length - 1; i >= 0; i--) {
+			System.out.print(array[i]);
+			if (i > 0) {
+				System.out.print(",");
+			}
+		}
+		System.out.flush();
 	}
-	
+
 	/**
-	 * Método deve imprimir os elementos primos de um array de inteiros. O formato da impressão 
-	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os elementos e 
+	 * Método deve imprimir os elementos primos de um array de inteiros. O formato
+	 * da impressão
+	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os
+	 * elementos e
 	 * após o último elemento. Utilize System.out.print()
-	 * @param array
+	 * 
+	 * @arrayam array
 	 */
-	public static void imprimirElementosPrimos(int[] array) 
-	{
-		System.out.print("");
+	public static void imprimirElementosPrimos(int[] array) {
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == 2) {
+				System.out.print(array[i] + " ");
+			} else if (array[i] > 2) {
+				boolean isarray = true;
+				for (int j = 2; j <= Math.sqrt(array[i]); j++) {
+					if (array[i] % j == 0) {
+						isarray = false;
+						break;
+					}
+				}
+				if (isarray) {
+					System.out.print(array[i] + " ");
+				}
+			}
+		}
 	}
-	
-	
+
 	/**
-	 * Método deve imprimir os elementos ímpares de um array de inteiros. O formato da impressão 
-	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os elementos e 
+	 * Método deve imprimir os elementos ímarrayes de um array de inteiros. O
+	 * formato
+	 * da impressão
+	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os
+	 * elementos e
 	 * após o último elemento. Utilize System.out.print()
-	 * @param array
+	 * 
+	 * @arrayam array
 	 */
-	public static void imprimirElementosImpares(int[] array) 
-	{
-		System.out.print("");
+	public static void imprimirElementosImpares(int[] array) {
+	    for (int elemento : array) {
+	        if (elemento % 2 != 0) {
+	            System.out.print(elemento + " ");
+	        }
+	    }
 	}
-	
-	
+
 	/**
-	 * Método deve imprimir os elementos pares de um array de inteiros. O formato da impressão 
-	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os elementos e 
+	 * Método deve imprimir os elementos arrayes de um array de inteiros. O formato
+	 * da
+	 * impressão
+	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os
+	 * elementos e
 	 * após o último elemento. Utilize System.out.print()
-	 * @param array
+	 * 
+	 * @arrayam array
 	 */
-	public static void imprimirElementosPares(int[] array) 
-	{
-		System.out.print("");
+	public static void imprimirElementosPares(int[] array) {
+	   
+		for (int i = 0; i < array.length; i++) {
+	        if (array[i] % 2 == 0) {
+	            System.out.print(array[i] + " ");
+	        }
+	    }
 	}
-	
-	
+
 	/**
-	 * Método deve imprimir o maior e o menor elemento de um array de inteiros. O formato da impressão 
-	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os elementos e 
+	 * Método deve imprimir o maior e o menor elemento de um array de inteiros. O
+	 * formato da impressão
+	 * deve ser feito numa única linha, utilizando espaço em branco (" ") entre os
+	 * elementos e
 	 * após o último elemento. Utilize System.out.print()
-	 * @param array
+	 * 
+	 * @arrayam array
 	 */
-	public static void imprimirMaiorMenorElemento(int[] array) 
-	{
-		System.out.print("");
+	public static void imprimirMaiorMenorElemento(int[] array) {
+
+	    int maior = Integer.MIN_VALUE;
+	    int menor = Integer.MAX_VALUE;
+
+	    for (int i = 0; i < array.length; i++) {
+
+	        System.out.print(array[i] + " ");
+
+	        if (array[i] > maior) {
+	            maior = array[i];
+	        }
+	        if (array[i] < menor) {
+	            menor = array[i];
+	        }
+	    }
+
+	    System.out.println("\n\nMaior numero da lista: " + maior);
+	    System.out.println("\nMenor numero da lista: " + menor);
+	    System.out.println("\n=========================================");
 	}
-	
+
+
 	/**
-	 * O método calcula a média aritmética dos elementos de um array de inteiros. 
+	 * O método calcula a média aritmética dos elementos de um array de inteiros.
 	 * Como resultado, o valor da média deve ser retornado no método.
-	 * @param array
+	 * 
+	 * @arrayam array
 	 * @return media
 	 */
-	public static float calcularMediaAritmetica(int[] array) 
-	{
-		return 0f;
+	public static float calcularMediaAritmetica(int[] array) {
+
+		float soma = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			soma = soma + array[i];
+			System.out.print(array[i] + " ");
+		}
+
+		float media = soma / array.length;
+
+		System.out.println("\n\nSoma valores Array: " + soma);
+		System.out.println("Numero valores: " + array.length);
+		System.out.println("\nMedia total: " + media);
+		System.out.println("\n=========================================");
+
+		return media;
 	}
-	
+
 }
--- src/org/listaalgoritmos/template/test/TesteDeUnidadeLista.java
+++ src/org/listaalgoritmos/template/test/TesteDeUnidadeLista.java
@@ -18,12 +18,12 @@ public class TesteDeUnidadeLista {
 	public void testQuestaoUmListaUm() {
 		Assert.assertTrue(false);
 	}
-	
+
 	@Test
 	public void testQuestaoDoisListaUm() {
-		Assert.assertEquals(26f,Math.round(RespostaListaExercicio.calcularImc(80f, 1.75f)), 0);
+		Assert.assertEquals(26f, Math.round(RespostaListaExercicio.calcularImc(80f, 1.75f)), 0);
 	}
-	
+
 	@Test
 	public void testQuestaoTresListaUm() {
 		Assert.assertEquals(75f, RespostaListaExercicio.calcularAreaTrapezio(20f, 10f, 5f), 0);
@@ -34,22 +34,22 @@ public class TesteDeUnidadeLista {
 		Assert.assertEquals(5, RespostaListaExercicio.maiorEntreDoisInteiros(5, 2));
 		Assert.assertEquals(5, RespostaListaExercicio.maiorEntreDoisInteiros(2, 5));
 	}
-	
+
 	@Test
 	public void testQuestaoCincoListaUm() {
 		Assert.assertTrue(RespostaListaExercicio.verificarParImpar(10));
 		Assert.assertFalse(RespostaListaExercicio.verificarParImpar(5));
 	}
-	
+
 	@Test
 	public void testQuestaoUmListaDois() {
-		float[] notas = {10,9,8,7,6};
-		Assert.assertEquals(8f,RespostaListaExercicio.calcularMediaNotas(notas), 0);
+		float[] notas = { 10, 9, 8, 7, 6 };
+		Assert.assertEquals(8f, RespostaListaExercicio.calcularMediaNotas(notas), 0);
 	}
-	
+
 	@Test
 	public void testQuestaoDoisListaDois() {
-		int[] arrayOrig = {6,5,4,3,2,1};
+		int[] arrayOrig = { 6, 5, 4, 3, 2, 1 };
 		String resultado = "1,2,3,4,5,6";
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(baos);
@@ -58,9 +58,8 @@ public class TesteDeUnidadeLista {
 		RespostaListaExercicio.imprimirArrayInverso(arrayOrig);
 		System.setOut(old);
 		Assert.assertTrue(resultado.equals(baos.toString()));
-		
 	}
-	
+
 	@Test
 	public void testQuestaoTresListaDois() {
 		Assert.assertTrue(false);
@@ -68,46 +67,49 @@ public class TesteDeUnidadeLista {
 
 	@Test
 	public void testQuestaoQuatroListaDois() {
-		int[] arrayOrig = {1,2,3,4,5,6,7,8,9,10,11};
+		int[] arrayOrig = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 		String resultado = "2 3 5 7 11 ";
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(baos);
 		PrintStream old = System.out;
 		System.setOut(ps);
-		RespostaListaExercicio.imprimirElementosPrimos(arrayOrig);;
+		RespostaListaExercicio.imprimirElementosPrimos(arrayOrig);
+		;
 		System.setOut(old);
 		Assert.assertTrue(resultado.equals(baos.toString()));
 	}
-	
+
 	@Test
 	public void testQuestaoCincoListaDois() {
-		int[] arrayOrig = {1,2,3,4,5,6,7,8,9,10,11};
+		int[] arrayOrig = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 		String resultado = "1 3 5 7 9 11 ";
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(baos);
 		PrintStream old = System.out;
 		System.setOut(ps);
-		RespostaListaExercicio.imprimirElementosImpares(arrayOrig);;
+		RespostaListaExercicio.imprimirElementosImpares(arrayOrig);
+		;
 		System.setOut(old);
 		Assert.assertTrue(resultado.equals(baos.toString()));
 	}
-	
+
 	@Test
 	public void testQuestaoSeisListaDois() {
-		int[] arrayOrig = {1,2,3,4,5,6,7,8,9,10,11};
+		int[] arrayOrig = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 		String resultado = "2 4 6 8 10 ";
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(baos);
 		PrintStream old = System.out;
 		System.setOut(ps);
-		RespostaListaExercicio.imprimirElementosPares(arrayOrig);;
+		RespostaListaExercicio.imprimirElementosPares(arrayOrig);
+		;
 		System.setOut(old);
 		Assert.assertTrue(resultado.equals(baos.toString()));
 	}
-	
+
 	@Test
 	public void testQuestaoSeteListaDois() {
-		int[] arrayOrig = {5,10,33,4,1,27,71,38,19,1,9,9};
+		int[] arrayOrig = { 5, 10, 33, 4, 1, 27, 71, 38, 19, 1, 9, 9 };
 		String resultado = "71 1 ";
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(baos);
@@ -117,10 +119,10 @@ public class TesteDeUnidadeLista {
 		System.setOut(old);
 		Assert.assertTrue(resultado.equals(baos.toString()));
 	}
-	
+
 	@Test
 	public void testQuestaoOitoListaDois() {
-		int[] arrayOrig = {15,33,4,1,27,71,38,19,1,9,9,13};
+		int[] arrayOrig = { 15, 33, 4, 1, 27, 71, 38, 19, 1, 9, 9, 13 };
 		Assert.assertEquals(20f, RespostaListaExercicio.calcularMediaAritmetica(arrayOrig), 0);
 	}
 
